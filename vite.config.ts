143a8fb98286cf5fc9a02929743284e0d5d22fd5
dom <35171942+domluther@users.noreply.github.com>
2026-03-25 17:53:10 +0000

    chore: updated packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
